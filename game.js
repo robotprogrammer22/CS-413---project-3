a85d08da978e1611d9acb56c353132da7fa067be
robotprogrammer22 <46390033+robotprogrammer22@users.noreply.github.com>
2019-10-26 14:23:04 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,170 @@
+//set up gameport, renderer, and stage
+var gameport = document.getElementById("gameport");
+//var renderer = PIXI.autoDetectRenderer(200, 500, {backgroundColor: 0x000000});
+var app = new PIXI.Application({width: 800, height: 500});
+gameport.appendChild(app.view);
+
+//scene graphs
+var titleScene;
+var gameScene;
+//var creditScene;
+var menuScene;
+var gameOverScene;
+
+var newgame;
+
+//title text
+var titleText;
+var startText;
+
+//game text
+var menuText;
+
+//menu text
+var titleReturnText;
+var resetText;
+var exitText;
+
+var gameOverText;
+
+PIXI.Loader.shared.add("assets.json").load(setup);
+
+//set up game
+function setup()
+{
+  titleScene = new PIXI.Container();
+  gameScene = new PIXI.Container();
+  menuScene = new PIXI.Container();
+  gameOverScene = new PIXI.Container();
+
+  newgame = new GameController(gameScene, 800, 500);
+
+  app.stage.addChild(titleScene);
+  app.stage.addChild(gameScene);
+  app.stage.addChild(menuScene); //this line breaks the ghost
+  app.stage.addChild(gameOverScene);
+
+  gameScene.visible = false;
+  menuScene.visible = false;
+  gameOverScene.visible = false;
+
+  titleSetup();
+  gameSetup();
+  menuSetup();
+  gameOverSetup();
+}
+
+function titleSetup()
+{
+  titleText = new PIXI.Text("Untitled Trick or Treat Game", {fill : 0xff1010});
+  startText = new PIXI.Text("Start", {fill : 0xff1010});
+
+  titleScene.addChild(titleText);
+  titleScene.addChild(startText);
+
+  titleText.position.x = 200;
+  titleText.position.y = 200;
+  startText.position.x = 200;
+  startText.position.y = 250;
+
+  startText.interactive = true;
+  startText.buttonMode = true;
+
+  startText.on('mousedown', dispGame);
+}
+
+function gameSetup()
+{
+  //newgame.runGame();
+  menuText =  new PIXI.Text("Menu", {fill : 0x000000});
+
+  menuText.interactive = true;
+  menuText.buttonMode = true;
+
+  menuText.on('mousedown', dispMenu);
+}
+
+function menuSetup()
+{
+  titleReturnText = new PIXI.Text("Return to title screen", {fill : 0xff1010});
+  resetText = new PIXI.Text("Start Over", {fill : 0xff1010});
+  exitText = new PIXI.Text("Exit", {fill : 0xff1010});
+
+  menuScene.addChild(titleReturnText);
+  menuScene.addChild(resetText);
+  menuScene.addChild(exitText);
+
+  titleReturnText.position.x = 200;
+  titleReturnText.position.y = 200;
+  resetText.position.x = 200;
+  resetText.position.y = 250;
+  exitText.position.x = 200;
+  exitText.position.y = 300;
+
+  titleReturnText.interactive = true;
+  titleReturnText.buttonMode = true;
+  resetText.interactive = true;
+  resetText.buttonMode = true;
+  exitText.interactive = true;
+  exitText.buttonMode = true;
+
+  titleReturnText.on('mousedown', dispTitle);
+  resetText.on('mousedown', resetGame);
+  exitText.on('mousedown', dispGame);
+}
+
+function gameOverSetup()
+{
+  gameOverText = new PIXI.Text("Game Over", {fill : 0xff1010});
+
+  gameOverScene.addChild(gameOverText);
+}
+
+function dispTitle()
+{
+  titleScene.visible = true;
+  gameScene.visible = false;
+  menuScene.visible = false;
+}
+
+function dispGame()
+{
+  titleScene.visible = false;
+  gameScene.visible = true;
+  menuScene.visible = false;
+  newgame.runGame();
+
+
+  gameScene.addChild(menuText);
+  menuText.position.x = 720;
+  menuText.position.y = 0;
+}
+
+function dispMenu()
+{
+  gameScene.visible = false;
+  menuScene.visible = true;
+}
+
+function dispGameOver()
+{
+  gameScene.visible = false;
+  gameOverScene.visible = true;
+}
+
+function resetGame()
+{
+  newgame.runGame();
+  dispGame();
+}
+
+function animate()
+{
+  requestAnimationFrame(animate);
+
+  if (!(newgame.isActive()))
+  {
+    dispGameOver();
+  }
+}
+animate();
---
+++ matchingGame.js
@@ -0,0 +1,153 @@
+class matchingGame
+{
+	constructor(container)
+	{
+		this.matching_container = new PIXI.Container();
+		this.existing_stage = container;
+		this.existing_stage.addChild(this.matching_container);
+		
+		this.game_finished = false;
+		this.score = 5;
+		
+		this.cards = [];
+		this.flipped_cards = 0;
+		this.remaining_cards = 6;
+	}
+	
+	startGame()
+	{
+		//setBoard();
+		//playGame();
+		return this.score;
+	}
+	
+	playGame()
+	{
+		// will contain logic for the matching game
+		
+		if (this.flipped_cards === 2)
+		{
+			checkForMatch();
+		}
+	}
+	
+	setBoard()
+	{
+		this.card_types = ["cat", "wolf", "tree"];
+		this.card_type_numbers = [0, 0, 0];
+		this.cards = 0;
+		
+		while (this.cards < 6)
+		{
+			this.random_card = Math.round(((Math.random()) * 2) + 1);
+			
+			if (this.random_card === 1)
+			{
+				// make cat
+				if (this.cards[0] < 2)
+				{
+					this.new_card = new Card("cat");
+					this.cards.push(this.new_card);
+					this.card_type_numbers[0] += 1;
+				}
+			}
+			else if (this.random_card === 2)
+			{
+				// make wolf
+				if (this.cards[1] < 2)
+				{
+					this.new_card = new Card("wolf");
+					this.cards.push(this.new_card);
+					this.card_type_numbers[1] += 1;
+				}
+			}
+			else (this.random_card === 3)
+			{
+				// make tree
+				if (this.cards[2] < 2)
+				{
+					this.new_card = new Card("tree");
+					this.cards.push(this.new_card);
+					this.card_type_numbers[2] += 1;
+				}
+			}
+		}
+		
+		// need 6 total cards
+		// need to check that 2 of each card exist, so use random to choose between 1-3 to randomly select the card type
+		// if that card type already has 2, then run again
+		// need to place the cards in the proper position here too
+		
+		
+	}
+	
+	/*
+	this.getUserInput = setInterval(function(){
+		if (bug_count >= 5)
+		{
+			bug_x_square = Math.round(((Math.random()) * 7) + 1);
+			bug_y_square = Math.round(((Math.random()) * 7) + 1);
+	
+			new_bug_x = bug_x_square * 40 + 20;
+			new_bug_y = bug_y_square * 40 + 20;
+		
+			createjs.Tween.get(bug).to({x: new_bug_x, y: new_bug_y}, 1000).call(tweenFinish, [new_bug_x, new_bug_y]);
+		}
+	}, 3000);
+	*/
+	
+	
+}
+
+
+
+class Card
+{	
+	constructor(type_input)
+	{
+		this.card_type = type_input;
+		this.flipped = false;
+		this.times_flipped = 0;
+		
+		this.cat_pic = PIXI.Texture.fromImage("Sprites/cat");
+		this.wolf_pic = PIXI.Texture.fromImage("Sprites/wolf");
+		this.tree_pic = PIXI.Texture.fromImage("Sprites/tree");
+		this.pumpkin = PIXI.Texture.fromImage("Sprites/pumpkin");
+		
+		this.card_front;
+		this.card_back = new PIXI.Sprite(pumpkin);
+		this.card_pic;
+		
+		if (type_input === "cat")
+		{
+			this.card_front = new PIXI.Sprite(cat_pic);
+			this.card_pic = "cat";
+		}
+		else if (type_input === "wolf")
+		{
+			this.card_front = new PIXI.Sprite(wolf_pic);
+			this.card_pic = "wolf";
+		}
+		else
+		{
+			this.card_front = new PIXI.Sprite(tree_pic);
+			this.card_pic = "tree";
+		}
+		
+		this.matching_container.addChild(this.card_back);
+		
+	}
+	
+	flipCard()
+	{
+		if (this.flipped === false)
+		{
+			// look up visible property for sprites
+		}
+		else
+		{
+			
+		}
+	}
+	
+}
